36c59f9f449afa37800a6726fd03798b4a1faa17
ahmed_Elkafrawy <ahmed@ahmed.com>
2017-09-16 23:15:33 +0200

    finishing login page

--- index.php
+++ index.php
@@ -4,4 +4,19 @@
 ?>
 <?php include "header.php"; ?>
 
+<div class="container">
+    <div class="row">
+         
+            <div class="login col-lg-12">
+
+                <div class="form-group col-lg-4"> 
+                        <label class="username-label">Enter Your Name : </label>
+                </div>
+                <div class="form-group col-lg-8">
+                    <input type="text" class="username form-control" name="username" placeholder="Username........"/>
+                </div>
+            </div>
+      
+    </div>    
+</div>    
 <?php  include "footer.php"; ?>
